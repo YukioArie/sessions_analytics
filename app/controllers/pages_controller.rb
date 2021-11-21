63ccffd87ae98fea080b55256cf83c3800b16abe
Yukio Arie <yukioarie@gmail.com>
2021-11-21 08:32:50 -0300

    add create_event from PagesController

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,18 @@
 class PagesController < ApplicationController
   before_action :set_page
+  before_action :create_event
   def show
     console
   end
 
   private
+
+  def create_event
+    Event.create!(
+      url: request.path,
+      visitorId: cookies[:visitorId]
+    )
+  end
 
   def set_page
     @page = Page.where(title: params[:title]).first
