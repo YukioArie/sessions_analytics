0a84896fd9f9d6ff5943ae0980e6118b08ea8949
Yukio Arie <yukioarie@gmail.com>
2021-11-21 06:29:08 -0300

    edit controller pages action show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
+  before_action :set_page
   def show
+    console
+  end
+
+  private
+
+  def set_page
+    @page = Page.where(title: params[:title]).first
   end
 end
