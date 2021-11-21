c3200460ec8719d465ed4188e9befb6934b78fd8
Yukio Arie <yukioarie@gmail.com>
2021-11-21 08:17:34 -0300

    generate model events

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  before_action :set_cookies
+
+  def set_cookies
+    cookies[:visitorId] = SecureRandom.uuid unless cookies[:visitorId]
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_082120) do
+ActiveRecord::Schema.define(version: 2021_11_21_111653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string "url"
+    t.string "visitorId"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
