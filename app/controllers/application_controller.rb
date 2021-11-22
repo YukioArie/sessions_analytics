de0d2e0d62802e58380b5ff8d9f2304a09cbb486
Yukio Arie <yukioarie@gmail.com>
2021-11-22 08:28:38 -0300

    add method to create user by cookies

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
-  before_action :set_cookies
-
-  def set_cookies
-    cookies[:visitorId] = SecureRandom.uuid unless cookies[:visitorId]
+  before_action :set_cookies_and_user
+  def set_cookies_and_user
+    unless cookies[:visitorId]
+      cookies[:visitorId] = SecureRandom.uuid
+      User.create!(visitorId: cookies[:visitorId])
+    end
   end
 end
