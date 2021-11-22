013ce3fcced5a74e70dad20fe1d1d096387b8017
Yukio Arie <yukioarie@gmail.com>
2021-11-22 08:28:06 -0300

    generate model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_21_111653) do
+ActiveRecord::Schema.define(version: 2021_11_22_112001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2021_11_21_111653) do
     t.string "visitorId"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "pages", force: :cascade do |t|
@@ -28,4 +30,11 @@ ActiveRecord::Schema.define(version: 2021_11_21_111653) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "visitorId"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "events", "users"
 end
